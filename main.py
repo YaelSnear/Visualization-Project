310344fc3d6e0f33c652e6765189295d4065cee4
rabaevn <rabaevn@post.bgu.ac.il>
2025-01-21 15:51:17 +0200

    Add files via upload
    all visualizations.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import zipfile
 import plotly.express as px
 import streamlit as st
 import pandas as pd
@@ -5,6 +6,11 @@ import requests
 import matplotlib.pyplot as plt
 import seaborn as sns
 import io
+import geopandas as gpd
+from io import BytesIO
+import base64
+import fiona
+
 
 #set page config
 st.set_page_config(page_title="Crime Dashboard", layout="wide")
@@ -67,8 +73,35 @@ def preprocess_data_district(df):
 
     return combined_df
 
+def extract_zip():
+    # נתיב לקובץ ה-ZIP שהועלה
+    zip_path = "policestationboundaries.gdb.zip"
+
+    # חלץ את התוכן לתוך קולאב
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall("PoliceStationBoundaries")
+
+    # ודא שהתיקייה קיימת
+    gdb_path = "PoliceStationBoundaries/PoliceStationBoundaries.gdb"
+    return gdb_path
+
+st.markdown("""
+    <style>
+    .block-container {
+        text-align: right;  /* יישור כל האלמנטים לימין */
+    }
+    div[data-baseweb="select"] > div {
+        direction: rtl;  /* שינוי כיוון ל-RTL */
+        text-align: right; /* יישור לימין */
+    }
+    .stCheckbox label {
+        direction: rtl;  /* שינוי כיוון ל-RTL */
+        text-align: right; /* יישור לימין */
+    }
+    </style>
+""", unsafe_allow_html=True)
 # Streamlit layout
-st.title("Crime Analysis Dashboard")
+st.title("פשיעה בארץ ישראל")
 
 # side-bar navigation
 menu_option = st.sidebar.radio("Select Visualization:", ["Overview", "Heatmap", "October 7th"])
@@ -79,11 +112,20 @@ if menu_option == 'Overview':
     # Determine Y-axis max value before filtering
     years = ["All Years (mean)"] + sorted(df["Year"].dropna().unique().astype(int).tolist())
     # Sidebar filters
-    year_selected = st.selectbox("Select Year:", years, index=0)
+    year_selected = st.selectbox(":בחר שנה", years, index=0)
     st.markdown("""
         <style>
         .stSelectbox [data-baseweb="select"] {
             width: 200px !important;
+            text-align: right;
+            direction: rtl;
+        }
+        .stCheckbox label {
+            text-align: right;  /* Align checkbox label to the right */
+            direction: rtl;
+        }
+        .plotly-title {
+            text-align: right !important;  /* Align plotly title to the right */
         }
         </style>
     """, unsafe_allow_html=True)
@@ -238,3 +280,60 @@ elif menu_option == 'October 7th':
 
     # Display bar chart
     st.plotly_chart(fig)
+
+elif menu_option == 'Heatmap':
+
+    gdb_path = extract_zip()
+    df = pd.read_csv('clean_df_heatmap.csv')
+    layer_name = "PoliceMerhavBoundaries"
+    gdf = gpd.read_file(gdb_path, layer=layer_name)
+
+    sorted_crimes = sorted(df['StatisticGroup'].dropna().unique(), key=lambda x: x) # sort crimes in alphabetical order
+    sorted_crimes = ['all_crimes'] + sorted_crimes
+
+    # visualization
+    # Title
+    st.title("מפת חום - עבירות משטרת ישראל")
+
+    # Dropdown for crime type
+    selected_crime = st.selectbox(
+        "בחר סוג עבירה:",
+        options=sorted_crimes,
+        index=0  # Default to 'all crimes'
+    )
+
+    # Dropdown for year
+    selected_year = st.selectbox(
+        "בחר שנה:",
+        options=['לאורך כל השנים', 2020, 2021, 2022, 2023, 2024],
+        index=0  # Default to 'all years'
+    )
+
+    # Filter data based on user selection
+    if selected_crime == 'all_crimes':
+        # Group and sum all crimes by PoliceMerhav
+        merhav_counts = df.groupby('PoliceMerhav').size()
+    else:
+        filtered_df = df[df['StatisticGroup'] == selected_crime]
+        merhav_counts = filtered_df['PoliceMerhav'].value_counts()
+
+    # Map the counts to the GeoDataFrame
+    gdf['record_count'] = gdf['MerhavName'].map(merhav_counts).fillna(0)
+
+    # Generate the heatmap
+    fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+    gdf.plot(
+        column='record_count',
+        ax=ax,
+        legend=True,
+        legend_kwds={'label': "Number of Records by Police Merhav", 'orientation': "horizontal"},
+        cmap='YlOrRd',
+        edgecolor='black'
+    )
+    ax.set_title(
+        f"Heatmap of {selected_crime} for {selected_year}" if selected_year != 'לאורך כל השנים' else f"Heatmap of {selected_crime} (All Years)")
+    ax.set_xlabel("Longitude")
+    ax.set_ylabel("Latitude")
+
+    # Display the heatmap in Streamlit
+    st.pyplot(fig)
